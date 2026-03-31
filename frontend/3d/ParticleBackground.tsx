348eb3d9323f021d8f737f252456d921e805a141
[PRVR-KSHR] <praveerkishore456@gmail.com>
2026-03-31 20:14:39 +0530

    Fix: suppress TypeScript error for maath module missing declarations

--- frontend/3d/ParticleBackground.tsx
+++ frontend/3d/ParticleBackground.tsx
@@ -3,6 +3,7 @@
 import React, { useRef, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 interface ParticlesProps {
